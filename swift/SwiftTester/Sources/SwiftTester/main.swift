9f0b7beadbda011ded6e93ad1c9e223fd2203884
Eric Betts <bettse@fastmail.fm>
2022-01-17 14:41:13 -0800

    Add more example code

--- swift/SwiftTester/Sources/SwiftTester/main.swift
+++ swift/SwiftTester/Sources/SwiftTester/main.swift
@@ -3,11 +3,30 @@ import CPm3
 print("This is written in Swift!")
 
 let port = "/dev/tty.usbmodemiceman1"
+let cmd = "help"
 
-//pm3 *pm3_open(char *port);
+/*
+pm3 *pm3_open(char *port)
+int pm3_console(pm3 *dev, char *cmd);
+const char *pm3_name_get(pm3 *dev);
+void pm3_close(pm3 *dev);
+pm3 *pm3_get_current_dev(void);
+*/
 
 port.withCString { port in
-    let _ = pm3_open(UnsafeMutablePointer(mutating: port));
+    let pm3 = pm3_open(UnsafeMutablePointer(mutating: port));
     print("Opened")
+    let name = String(cString: pm3_name_get(pm3))
+    print("name: \(name)")
+
+
+    print("Running `\(cmd)`")
+    cmd.withCString { cmd in
+        let result = pm3_console(pm3, UnsafeMutablePointer(mutating: cmd));
+        print("Result of command: \(result)")
+    }
+
+    pm3_close(pm3)
+    print("Closed")
 }
 
